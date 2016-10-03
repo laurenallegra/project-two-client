b414ddb515068b6752526775dc01765607869b4a
laurenallegra <lauren.allegra@gmail.com>
2016-10-03 15:23:22 -0400

    Begin to build out basic auth feature
    -add `auth` folder with 3 files (below), following same folder/file
    structure as project one (front-end) template:
      -api.js
      -events.js
      -ui.js
    -add app.js file in `scripts` folder with host set to
    `http://localhost:8080/` to test functionality locally before deploying

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const app = {
+  host: 'http://localhost:8080',
+};
+
+module.exports = app;
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const app = require('./app');
+
+const clearForm = (formId) => {
+  document.getElementById(formId).reset();
+};
+
+const toggleAuth = () => {
+  $('.auth-nav').toggle();
+  $('.auth-forms').toggle();
+  $('.user-nav').toggle();
+  $('.game-play').toggle();
+};
+
+// const toggleAuthOptions = () => {
+//   $('.sign-up-link').toggleClass('hidden');
+//   $('.log-in-link').toggleClass('block');
+//   $('#log-in').toggle('fast');
+//   $('#sign-up').toggle('fast');
+// };
+
+
+const toggleChangePassword = () => {
+  $('.pwd-form').slideToggle();
+};
+
+// const logInFailure = () => {
+//   messageFade('.messages div', 'logInFail');
+// };
+//
+// const logOutFailure = () => {
+//   messageFade('.messages div', 'logOutFail');
+// };
+//
+// const passwordChangeFailure = () => {
+//   messageFade('.messages div', 'passwordChangeFail');
+// };
+//
+// const signUpFailure = () => {
+//   messageFade('.messages div', 'signUpFail');
+// };
+
+const logInSuccess = (data) => {
+  app.user = data.user;
+  toggleAuth();
+  clearForm('sign-up');
+  clearForm('log-in');
+};
+
+const logOutSuccess = () => {
+  app.user = null;
+  toggleAuth();
+  clearForm('change-password');
+  $('.pwd-form').hide();
+  $('.sign-up-link').removeClass('hidden');
+  $('.log-in-link').addClass('hidden');
+  $('#log-in').show();
+  $('#sign-up').hide();
+};
+
+
+module.exports = {
+  clearForm,
+  toggleAuth,
+  toggleChangePassword,
+  // logInFailure,
+  // logOutFailure,
+  // passwordChangeFailure,
+  // signUpFailure,
+  logInSuccess,
+  logOutSuccess,
+};
